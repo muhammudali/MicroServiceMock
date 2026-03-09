a0902e5a5f62fb5ccd631eecdc7fd82ac0499439
muhammudali <muhammudali@hotmail.com>
2026-03-09 12:09:59 +0000

    Update main.tf

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -8,11 +8,6 @@ variable "image_url" {
   type        = string
 }
 
-resource "google_artifact_registry_repository" "my-repo" {
-  location      = "europe-west2"
-  repository_id = "microservices"
-  format        = "DOCKER"
-}
 
 resource "google_cloud_run_v2_service" "default" {
   name     = "hello-world-service"
